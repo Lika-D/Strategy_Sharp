f830cf7365028c442b9d1782ffea5b98a081b455
Lika-D <55454606+Lika-D@users.noreply.github.com>
2019-10-01 20:02:38 +0300

    Hats_Mods
    /*
     * Создано в SharpDevelop.
     * Пользователь: Student
     * Дата: 01.10.2019
     * Время: 19:44
     * 
     * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
     */
    using System;
    
    namespace Cap_Pro
    {
    	class Program
    {
        static void Main(string[] args)
        {
            Cap hat = new Cap(8, "Vip", new DemoSeasonCold());
            hat.Season();
            hat.Weather = new DemoSeasonWarm();
            hat.Season();
     
            Console.ReadLine();
        }
    }
    interface ISeasonsWeather
    {
        void Season();
    }
     
    class DemoSeasonCold : ISeasonsWeather
    {
        public void Season()
        {
            Console.WriteLine("Пригодна для носки зимой");
        }
    }
     
    class DemoSeasonWarm : ISeasonsWeather
    {
        public void Season()
        {
            Console.WriteLine("Пригодна для носки весной");
        }
    }
    class Cap
    {
        protected int wedge; // кол-во клиньев
        protected string model; // модель кепки
     
        public Cap(int num, string model, ISeasonsWeather mov)
        {
            this.wedge = num;
            this.model = model;
            Weather = mov;
        }
        public ISeasonsWeather Weather { private get; set; }
        public void Season()
        {
            Weather.Season();
        }
    }
    	}

---
+++ Hats_Pro.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Student
+ * Дата: 01.10.2019
+ * Время: 18:53
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace Dor_Proj
+{
+	class Program
+{
+    static void Main(string[] args)
+    {
+        Cap hat = new Cap(8, "Vip", new DemoSeasonCold());
+        hat.Season();
+        hat.Weather = new DemoSeasonWarm();
+        hat.Season();
+ 
+        Console.ReadLine();
+    }
+}
+interface ISeasonsWeather
+{
+    void Season();
+}
+ 
+class DemoSeasonCold : ISeasonsWeather
+{
+    public void Season()
+    {
+        Console.WriteLine("Пригодна для носки зимой");
+    }
+}
+ 
+class DemoSeasonWarm : ISeasonsWeather
+{
+    public void Season()
+    {
+        Console.WriteLine("Пригодна для носки весной");
+    }
+}
+class Cap
+{
+    protected int wedge; // кол-во клиньев
+    protected string model; // модель кепки
+ 
+    public Cap(int num, string model, ISeasonsWeather mov)
+    {
+        this.wedge = num;
+        this.model = model;
+        Weather = Season;
+    }
+    public ISeasonsWeather Weather { private get; set; }
+    public void Season()
+    {
+        Weather.Season();
+    }
+}
+	}
